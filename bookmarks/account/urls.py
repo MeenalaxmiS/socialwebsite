f75cf642be3586f9c0f2871eb43b4c60e7b2db6d
Meenalaxmi <meenalaxmi@testpress.in>
2021-09-20 06:01:21 +0530

    Add user registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
                 name='password_reset_confirm'),
         path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),
                 name='password_reset_complete'),
+        path("register/", views.register, name="register"),
 
     
 ]
